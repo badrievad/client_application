790c6b29da890b6e6ec09d8d65faa626ad81f055
badrievad <zzdl4@bk.ru>
2025-01-15 11:04:02 +0300

    fixed

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import subprocess
 import sys
@@ -20,6 +19,7 @@ CORS(app)
 
 # Загружаем текущую версию из файла
 def load_version():
+    """Загружает текущую версию приложения из файла version.txt."""
     try:
         with open("version.txt", "r") as file:
             return file.read().strip()
@@ -30,6 +30,7 @@ def load_version():
 
 # Сохраняем новую версию в файл
 def save_version(new_version):
+    """Сохраняет новую версию приложения в файл version.txt."""
     try:
         with open("version.txt", "w") as file:
             file.write(new_version)
@@ -41,6 +42,9 @@ def save_version(new_version):
 # Основной маршрут для открытия папок
 @app.route("/open-deal-folder", methods=["POST"])
 def open_folder():
+    """
+    Открывает указанную папку на клиентском компьютере.
+    """
     folder_path = request.json.get("folder_path")
     logging.info(f"Папка для открытия: {folder_path}")
     if folder_path and os.path.exists(folder_path):
@@ -59,7 +63,9 @@ def open_folder():
 
 # Функция проверки обновлений
 def check_for_updates():
-    """Проверяет наличие обновлений и загружает новую версию, если доступна."""
+    """
+    Проверяет наличие обновлений на сервере и, если доступны, скачивает и применяет их.
+    """
     while True:
         try:
             current_version = load_version()
@@ -67,6 +73,7 @@ def check_for_updates():
 
             response = requests.get(UPDATE_CHECK_URL)
             logging.info(f"Ответ от сервера: {response.status_code}")
+
             if response.status_code == 200:
                 data = response.json()
                 latest_version = data.get("version")
@@ -74,7 +81,7 @@ def check_for_updates():
 
                 if not latest_version or not download_url:
                     logging.warning(
-                        "Ответ от сервера некорректен. Нет версии или ссылки."
+                        "Некорректный ответ от сервера: отсутствуют версия или URL."
                     )
                     continue
 
@@ -84,13 +91,12 @@ def check_for_updates():
                     )
                     download_and_update(download_url, latest_version)
                 else:
-                    logging.info(
-                        f"Новых обновлений нет. Версия на сервере: {latest_version}"
-                    )
+                    logging.info(f"Вы используете последнюю версию: {current_version}.")
             else:
                 logging.warning(
                     f"Не удалось получить обновления. Статус: {response.status_code}"
                 )
+
         except requests.ConnectionError:
             logging.error("Ошибка соединения с сервером. Проверьте подключение.")
         except requests.Timeout:
@@ -98,36 +104,42 @@ def check_for_updates():
         except Exception as e:
             logging.error(f"Ошибка проверки обновлений: {e}")
 
-        time.sleep(20)  # Проверяем обновления каждый час
+        time.sleep(15)  # Проверяем обновления раз в час
 
 
 # Функция загрузки и установки новой версии
 def download_and_update(download_url, latest_version):
     """
-    Скачивает новую версию, сохраняет её, записывает новую версию в файл
-    и запускает обновление.
+    Скачивает новую версию приложения и перезапускает её.
     """
     try:
         new_version_path = f"client_app_v{latest_version}.exe"
         response = requests.get(download_url, stream=True)
         if response.status_code == 200:
-            with open(new_version_path, "wb") as f:
+            with open(new_version_path, "wb") as file:
                 for chunk in response.iter_content(chunk_size=1024):
-                    f.write(chunk)
+                    file.write(chunk)
             logging.info(f"Новая версия скачана: {new_version_path}")
+
+            # Сохраняем новую версию в файл version.txt
             save_version(latest_version)
-            logging.info(f"Новая версия {latest_version} сохранена в version.txt.")
-            subprocess.Popen([new_version_path, "--wait", "3"])
-            sys.exit(0)
+
+            # Запускаем новую версию и завершаем текущую
+            logging.info(f"Запуск новой версии: {new_version_path}")
+            subprocess.Popen([new_version_path])
+            sys.exit(0)  # Завершение текущего процесса
         else:
             logging.error(f"Ошибка загрузки файла. Статус: {response.status_code}")
     except Exception as e:
         logging.error(f"Ошибка загрузки обновления: {e}")
 
 
-# Запуск приложения
+# Основной запуск приложения
 if __name__ == "__main__":
+    # Запуск проверки обновлений в отдельном потоке
     threading.Thread(target=check_for_updates, daemon=True).start()
+
+    # Запуск Flask-приложения для обработки локальных запросов
     logging.info(f"Flask приложение запущено на порте 5001")
     try:
         app.run(host="localhost", port=5001)
